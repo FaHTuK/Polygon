9b08cb5be84ebda5c3288d8c2ba129ad0155c3f4
FaHTuK <davidkacuro@hotmail.com>
2018-06-30 22:22:54 +0700

    Add files via upload

---
+++ Polygon/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Polygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.polygonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemDraw = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.BackColor = System.Drawing.Color.White;
+            this.panelDraw.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelDraw.Location = new System.Drawing.Point(12, 27);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(482, 369);
+            this.panelDraw.TabIndex = 0;
+            this.panelDraw.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelDraw_MouseUp);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(500, 27);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(141, 48);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw polygon";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(500, 81);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(141, 48);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear panel";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 399);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(653, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(39, 17);
+            this.statusLabel.Text = "Status";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.polygonToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(653, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemSave,
+            this.itemLoad});
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.loadToolStripMenuItem.Text = "File";
+            // 
+            // itemSave
+            // 
+            this.itemSave.Name = "itemSave";
+            this.itemSave.Size = new System.Drawing.Size(180, 22);
+            this.itemSave.Text = "Save";
+            this.itemSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // itemLoad
+            // 
+            this.itemLoad.Name = "itemLoad";
+            this.itemLoad.Size = new System.Drawing.Size(180, 22);
+            this.itemLoad.Text = "Load";
+            this.itemLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // polygonToolStripMenuItem
+            // 
+            this.polygonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itemDraw,
+            this.itemClear});
+            this.polygonToolStripMenuItem.Name = "polygonToolStripMenuItem";
+            this.polygonToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.polygonToolStripMenuItem.Text = "Polygon";
+            // 
+            // itemDraw
+            // 
+            this.itemDraw.Name = "itemDraw";
+            this.itemDraw.Size = new System.Drawing.Size(148, 22);
+            this.itemDraw.Text = "Draw polygon";
+            this.itemDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // itemClear
+            // 
+            this.itemClear.Name = "itemClear";
+            this.itemClear.Size = new System.Drawing.Size(148, 22);
+            this.itemClear.Text = "Clear panel";
+            this.itemClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(500, 348);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(141, 48);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(500, 294);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(141, 48);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 421);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.panelDraw);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Polygon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDraw;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemSave;
+        private System.Windows.Forms.ToolStripMenuItem polygonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemDraw;
+        private System.Windows.Forms.ToolStripMenuItem itemClear;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ToolStripMenuItem itemLoad;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Polygon/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Polygon
+{
+    public partial class Form1 : Form
+    {
+        List<Point> pointsList = new List<Point>();
+
+        private Point[] m_aPolygon;
+        private ArrayList m_alSelectedPts = new ArrayList();
+        private Pen m_PolygonPen = new Pen(Color.Blue, 2);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Рисуем крестики (вершины) по клику на экране
+        private void DrawCross(int x, int y)
+        {
+            Graphics graphics = panelDraw.CreateGraphics();
+            Point startPoint = new Point(x - 2, y);
+            Point endPoint = new Point(x + 2, y);
+            graphics.DrawLine(Pens.Red, startPoint, endPoint);
+
+            startPoint = new Point(x, y - 2);
+            endPoint = new Point(x, y + 2);
+            graphics.DrawLine(Pens.Red, startPoint, endPoint);
+        }
+
+        //Соединяем вершины (в порядке их добавления) и рисуем полигон
+        private void DrawPolygon()
+        {
+            Read(); 
+
+            if (m_aPolygon != null)
+            {
+                if (m_aPolygon.Length > 0)
+                {
+                    Graphics gfx = this.panelDraw.CreateGraphics();
+                    gfx.DrawPolygon(m_PolygonPen, m_aPolygon);
+                }
+            }
+        }
+
+        //Считываем все вершины на панели
+        private void Read()
+        {
+            //Проверяем, чтобы крестиков было не менее 3-х штук
+            if (m_alSelectedPts.Count < 3)
+                MessageBox.Show("Please using mouse to pick at least 3 points!",
+                    "How to initialize a Polygon",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                //Получаем координаты всех вершин
+                m_aPolygon = new Point[m_alSelectedPts.Count];
+                for (int i = 0; i < m_alSelectedPts.Count; i++)
+                {
+                    m_aPolygon[i] = (Point)m_alSelectedPts[i];
+
+                    //Записываем координаты вершин в List
+                    pointsList.AddRange(new Point[] {
+                        new Point(m_aPolygon[i].X, m_aPolygon[i].Y)
+                    });
+                }
+            }
+        }
+
+        //Метод для проверки, находится ли точка внутри полигона или нет
+        public bool IsPointInPolygon(Point point, Point[] polygonPoints)
+        {
+            double x_min, x_max, y_min, y_max;
+
+            x_min = x_max = polygonPoints[0].X;
+            y_min = y_max = polygonPoints[0].Y;
+            for (int i = 1; i < polygonPoints.Length; i++)
+            {
+                Point p = polygonPoints[i];
+                x_min = Math.Min(p.X, x_min);
+                x_max = Math.Max(p.X, x_max);
+                y_min = Math.Min(p.Y, y_min);
+                y_max = Math.Max(p.Y, y_max);
+            }
+
+            if (point.X < x_min || point.X > x_max || point.Y < y_min || point.Y > y_max)
+            {
+                return false;
+            }
+
+            bool ins = false;
+            for (int i = 0, j = polygonPoints.Length - 1; i < polygonPoints.Length; j = i++)
+            {
+                if ((polygonPoints[i].Y > point.Y) != (polygonPoints[j].Y > point.Y) &&
+                     point.X < (polygonPoints[j].X - polygonPoints[i].X) * (point.Y - polygonPoints[i].Y) / (polygonPoints[j].Y - polygonPoints[i].Y) + polygonPoints[i].X)
+                {
+                    ins = !ins;
+                }
+            }
+
+            return ins;
+        }
+        
+        //Очищаем панель
+        private void Clear()
+        {
+            m_aPolygon = new Point[0];
+            m_alSelectedPts.Clear();
+            panelDraw.CreateGraphics().Clear(panelDraw.BackColor);
+            Invalidate();
+        }
+
+        //Рисуем вершины по клику
+        private void panelDraw_MouseUp(object sender, MouseEventArgs e)
+        {
+            Point clickedPt = new Point(e.X, e.Y);
+            m_alSelectedPts.Add(clickedPt);
+            DrawCross(e.X, e.Y);
+
+            if (btnDraw.Enabled == false)
+            {
+                //Выводим, находится ли точка внутри полигона
+                statusLabel.Text = "Is Point In Polygon: " + IsPointInPolygon(clickedPt, m_aPolygon).ToString();
+            }
+        }
+
+        //Соединяем вершины
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            DrawPolygon();
+            btnDraw.Enabled = false;
+            btnSave.Enabled = true;
+        }
+
+        //Очищаем панель
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+            btnDraw.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+            btnSave.Enabled = false;
+        }
+
+        //Сохраняем полигон в XML-файл
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Xml Files (*.xml)|*.xml";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Point>));
+                    xmlSerializer.Serialize(writer, pointsList);
+                }
+            }
+        }
+
+        //Загружаем полигон из XML-файла
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Xml Files (*.xml)|*.xml";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Clear();
+                using (StreamReader reader = new StreamReader(openFileDialog.FileName))
+                {
+                    XmlSerializer xml = new XmlSerializer(typeof(List<Point>));
+                    List<Point> point = (List<Point>)xml.Deserialize(reader);
+                    foreach (var a in point)
+                    {
+                        int x = a.X;
+                        int y = a.Y;
+                        Point loadPt = new Point(x, y);
+                        m_alSelectedPts.Add(loadPt);
+                        DrawCross(x, y);
+                    }
+                    DrawPolygon();
+                    btnDraw.Enabled = false;
+                }
+            }
+        }
+    }
+}
